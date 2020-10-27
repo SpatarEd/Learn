a551d24cbe098cfdc609abc49dfcc753b6e1a4aa
SpatarEd <73346494+SpatarEd@users.noreply.github.com>
2020-10-27 22:13:07 +0300

    Add files via upload

---
+++ TochkaVGrafike.java
@@ -0,0 +1,11 @@
+public class TochkaVGrafike {
+    public static void main(String[] args) {
+        // Проверить попадает ли точка в области на графике
+        int x = 48;
+        int y = 0;
+        boolean a = -4<=x && x<=4 && -3<=y && y<= 0 || -2<=x && x<=2 && 0<=y && y<=4;
+        System.out.println(a);
+
+
+    }
+}
